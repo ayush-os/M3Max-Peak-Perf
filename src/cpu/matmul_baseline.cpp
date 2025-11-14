e497aa236bbad4632bad0f56b0391aacfd9e5b5f
ayush <99374392+ayush-os@users.noreply.github.com>
2025-11-14 09:50:10 -0800

    naive

--- src/cpu/matmul_baseline.cpp
+++ src/cpu/matmul_baseline.cpp
@@ -1,3 +1,14 @@
-// m3max::aligned_unique_ptr<float> aligned_a(
-//     static_cast<float*>(m3max::aligned_alloc(64, sizeof(float) * num_elements_a))
-// );
+#include "utils/tensor.h"
+
+void baseline_matmul(const m3max::Tensor<float> &A,
+                     const m3max::Tensor<float> &B, m3max::Tensor<float> &C) {
+  for (size_t i = 0; i < C.rows(); i++) {
+    for (size_t j = 0; j < C.cols(); j++) {
+      float res = 0.0f;
+      for (size_t k = 0; k < A.cols(); k++) {
+        res += A(i, k) * B(k, j);
+      }
+      C(i, j) = res;
+    }
+  }
+}
